ad092ab3824977b1c9c114fb254633013aa014f4
Travis Benedict <Travisxbenedict@gmail.com>
2025-01-22 17:00:45 -0800

    finished lab 1

---
+++ lab1.py
@@ -0,0 +1,46 @@
+import math
+print(math.pi)
+print(" ")
+print("area")
+A = math.pi * 5**2
+print(A)
+print(" ")
+print("volume")
+V = (4/3) * math.pi * 3**3
+print(V)
+print(" ")
+print("hypotenuse")
+H = 3**2 + 4**2
+C = math.sqrt(H)
+print(C)
+print(" ")
+print("name length")
+first_name = "Travis"
+last_name = "Benedict"
+full_name = first_name + " " + last_name
+name_len = len(full_name)
+print(name_len)
+print(" ")
+print("string manipulation")
+print(full_name)
+print(full_name.upper())
+print(full_name.lower())
+print(" ")
+age = 26
+print("Age")
+print(age)
+print(" ")
+hi = (5 * 12) + 10
+hight = "5ft 10in"
+print("Hight")
+print(hight)
+print(" ")
+weight = 220
+print("Weight")
+print(weight)
+print(" ")
+print(type(age),type(hi),type(weight))
+print(" ")
+BMI = (weight / hi**2) * 703
+print("BMI")
+print(BMI)
